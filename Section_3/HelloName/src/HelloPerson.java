a762d321eaaafe0883a32731b470359e42109ed7
Jason Leong <n8329711@qut.edu.au>
2020-04-02 13:11:53 +1000

    Added HelloName + Variables
    Prints out a name
    Prints out a variable value (expression)

--- Section_3/HelloName/src/HelloPerson.java
+++ Section_3/HelloName/src/HelloPerson.java
@@ -1,5 +1,6 @@
 public class HelloPerson {
     public static void main(String[] args){
+        //Text inside "" called literal string
         System.out.println("Hello Knowing Bros");
     }
 }
---
+++ Section_3/Variables/src/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        //variables store information we provide and accessed through name and are found in computer (RAM)
+        //random access memory
+        //Variables can hold many different types of data know as data types
+
+        //data type - name - value(expression) that we initialize
+        //Form a declaration statement
+        int myFirstNumber = 5;
+
+        //typing sout tab is shortcut for below code
+        System.out.println(myFirstNumber);
+    }
+}
