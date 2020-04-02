4aa2e8e142a920f1f7f862d81f2da4392b6b6267
Jason Leong <n8329711@qut.edu.au>
2020-04-02 13:23:31 +1000

    ChallengeVariable
    Variables and expression

---
+++ Section_3/ChallengeVariable/src/ChallengeVariables.java
@@ -0,0 +1,12 @@
+public class ChallengeVariables {
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+        int mySecondNumber = 6;
+        int myThirdNumber = myFirstNumber + 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 500 - myTotal;
+
+        System.out.println(myTotal);
+        System.out.println(myLastOne);
+    }
+}
