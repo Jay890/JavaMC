34759300626949971f5ac86f03baf4726f554e55
Jason Leong <n8329711@qut.edu.au>
2020-04-07 16:27:47 +1000

    float and double
    Float and double examples

--- Section_3/primitiveTypeChallenge/src/com/company/Main.java
+++ Section_3/primitiveTypeChallenge/src/com/company/Main.java
@@ -9,5 +9,28 @@ public class Main {
 	    long validLong = 50_000L + (10L * (validByte + validShort + validInt));
 
         System.out.println(validLong);
+
+        //float - single precision type
+		float myMinFloatValue = Float.MIN_VALUE;
+		float myMaxFloatValue = Float.MAX_VALUE;
+		//double - double precision type
+		double myMinDoubleValue = Double.MIN_VALUE;
+		double myMaxDoubleValue = Double.MAX_VALUE;
+
+		System.out.println("Min Float Value: " + myMinFloatValue);
+		System.out.println("Max Float Value " + myMaxFloatValue);
+		System.out.println("Min Double Vlaue " + myMinDoubleValue);
+		System.out.println("Max Double Value " + myMaxDoubleValue);
+
+		//int is the default for whole numbers in Java
+		//double is the default for decimals in Java
+
+		int myIntValue = 2;
+//		float myFloatValue = (float) 2.2;
+		float myFloatValue2 =  2.2f;
+		double myDoubleValue = 2.2d;
+
+		//challenge
+
     }
 }
